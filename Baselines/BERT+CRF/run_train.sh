bbe0718cfc752efaeb97e6b2daafa4850dc83e65
CubicPower <55009831+markhsia@users.noreply.github.com>
2023-04-16 17:29:30 +0800

    Update run_train.sh

--- Baselines/BERT+CRF/run_train.sh
+++ Baselines/BERT+CRF/run_train.sh
@@ -13,6 +13,6 @@ CUDA_VISIBLE_DEVICES=4,5,6,7 python run_leven.py \
 	--seed 100\
 	--do_train \
 	--do_eval \
-	--model_name_or_path ./bert-base-chinese.bin \
+	--model_name_or_path ./bert-base-chinese \
   --eval_all_checkpoints \
   --overwrite_output_dir
